2ed376f0d42c9b7030f6301305480cb70cc57597
Ahmed Fahim <74918992+AhmedFahim-git@users.noreply.github.com>
2022-01-19 16:56:42 +0600

    Made build_features.py into a function that makes training and test sets in the data/processed directory. Prettified emotion.ipynb

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,19 +1,45 @@
-import pandas as pd
+#
+import random
+import shutil
 import os
+
+def get_working_dir():
+    dir_name = 'emotion_recognition'
+    cwd = os.getcwd()
+    wd = cwd[:cwd.index(dir_name)+len(dir_name)+1]
+    return wd
+
+emotions = ['happy','sad','angry', 'neutral', 'excited']
 
 def make_df():
     file_path = 'data/interim/raw jl corpus (unchecked and unannotated)/JL(wav+txt)/'
-    dir_name = 'emotion_recognition'
-    cwd = os.getcwd()
-    path = os.path.join(cwd[:cwd.index(dir_name)+len(dir_name)+1], *file_path.split('/'))
-
-    file_names = []
+    
+    wd = get_working_dir()
+    
+    path = os.path.join(wd, *file_path.split('/'))
+    
+    if not os.path.exists(os.path.join(wd, 'data','processed', 'train')):
+        os.makedirs(os.path.join(wd, 'data','processed', 'train'))
+        os.makedirs(os.path.join(wd, 'data', 'processed', 'test'))
+    
+    # file_names = []
+    random.seed(1234)
+    
     for file in os.listdir(path):
-        if file.endswith('.wav'):
-            file_names.append(file)
+        if file.endswith('.wav') and any([emotion in file for emotion in emotions]):
+            if random.random() < 0.8:
+                shutil.copyfile(os.path.join(path, file), os.path.join(wd, 'data', 'processed', 'train', file))
+            else:
+                shutil.copyfile(os.path.join(path, file), os.path.join(wd, 'data', 'processed', 'test', file))
+            # file_names.append(file)
 
-    df = pd.DataFrame({'file_names':file_names, 'emotion':[x.split('_')[1] for x in file_names]})
-    df['file_names'] = df['file_names'].apply(lambda x: os.path.join(path, x))
+    
+    
+    
+    # df = pd.DataFrame({'file_names':file_names, 'emotion':[x.split('_')[1] for x in file_names]})
+    # df['file_names'] = df['file_names'].apply(lambda x: os.path.join(path, x))
 
-    return df
+    return None
+
+make_df()
 
